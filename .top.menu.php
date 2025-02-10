453cc3dbbf84170a514b81bf727974c07b8d298d
Daniil Neretin <dneretin01@gmail.com>
2025-02-10 23:41:14 +0300

    add structure

---
+++ .top.menu.php
@@ -0,0 +1,24 @@
+<?php
+$aMenuLinks = array(
+    array(
+        "Новые поступления",
+        "#",
+        array(),
+        array(),
+        ""
+    ),
+    array(
+        "Повседневная одежда",
+        "#",
+        array(),
+        array(),
+        ""
+    ),
+    array(
+        "Мотоэкипировка",
+        "#",
+        array(),
+        array(),
+        ""
+    )
+);
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+/** @var \CMain $APPLICATION */
+
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Главная");
+?>
+
+<?php require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
