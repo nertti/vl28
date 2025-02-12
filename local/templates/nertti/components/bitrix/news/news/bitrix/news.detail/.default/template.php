a2324ea34efcd153f331b13fa0e9556faf46e5ad
Daniil Neretin <dneretin01@gmail.com>
2025-02-12 23:43:33 +0300

    add news template

---
+++ local/templates/nertti/components/bitrix/news/news/bitrix/news.detail/.default/template.php
@@ -0,0 +1,29 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+/** @var array $arParams */
+/** @var array $arResult */
+/** @global CMain $APPLICATION */
+/** @global CUser $USER */
+/** @global CDatabase $DB */
+/** @var CBitrixComponentTemplate $this */
+/** @var string $templateName */
+/** @var string $templateFile */
+/** @var string $templateFolder */
+/** @var string $componentPath */
+/** @var CBitrixComponent $component */
+$this->setFrameMode(true);
+?>
+<div class="container">
+<span class="date"><?= $arResult['DISPLAY_ACTIVE_FROM'] ?></span>
+<?=$arResult['~DETAIL_TEXT']?>
+<a href="<?=$arResult['PROPERTIES']['SOURCE']['~VALUE']?>" class="default__link default__link_small gray"><?=$arResult['PROPERTIES']['SOURCE']['NAME']?></a>
+</div>
+<?php if (!empty($arResult['PROPERTIES']['PHOTOS']['VALUE'])): ?>
+<div class="default__gallery">
+    <?php foreach($arResult['PROPERTIES']['PHOTOS']['VALUE'] as $arPhoto):?>
+        <img class="default__img default__img_s33" src="<?= CFile::GetPath($arPhoto); ?>" alt="Фото">
+    <?php endforeach;?>
+</div>
+<?php endif;?>
+<div class="container">
+    <a href="/news/" class="default__link default__link_news">Читать другие статьи</a>
+</div>
---
+++ local/templates/nertti/components/bitrix/news/news/bitrix/news.list/.default/template.php
@@ -0,0 +1,33 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+/** @var array $arParams */
+/** @var array $arResult */
+/** @global CMain $APPLICATION */
+/** @global CUser $USER */
+/** @global CDatabase $DB */
+/** @var CBitrixComponentTemplate $this */
+/** @var string $templateName */
+/** @var string $templateFile */
+/** @var string $templateFolder */
+/** @var string $componentPath */
+/** @var CBitrixComponent $component */
+$this->setFrameMode(true);
+?>
+
+<div class="blog__list">
+    <?php foreach($arResult["ITEMS"] as $arItem):?>
+    <?php
+    $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
+    $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
+    ?>
+    <a href="<?=$arItem['DETAIL_PAGE_URL']?>" class="article">
+        <img src="<?=$arItem['PREVIEW_PICTURE']['SRC']?>" alt="<?=$arItem['NAME']?>" class="article__img">
+        <div class="article__inner">
+            <p class="article__title"><?=$arItem['NAME']?></p>
+            <p class="article__desc">
+                <?=$arItem['~PREVIEW_TEXT']?>
+            </p>
+            <span class="article__date"><?=$arItem['ACTIVE_FROM']?></span>
+        </div>
+    </a>
+  <?php endforeach;?>
+</div>
