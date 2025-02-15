0c1fd88d3ec7eb10fa0f73e4ed26f119a6b676f4
Daniil Neretin <dneretin01@gmail.com>
2025-02-16 00:29:06 +0300

    fix bitrix parameters

--- local/templates/nertti/components/bitrix/news/news/bitrix/news.list/.default/template.php
+++ local/templates/nertti/components/bitrix/news/news/bitrix/news.list/.default/template.php
@@ -22,7 +22,7 @@ $this->setFrameMode(true);
     $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
     $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
     ?>
-    <a href="<?=$arItem['DETAIL_PAGE_URL']?>" class="article">
+    <a id="<?= $this->GetEditAreaId($arItem['ID']); ?>" href="<?=$arItem['DETAIL_PAGE_URL']?>" class="article">
         <img src="<?=$arItem['PREVIEW_PICTURE']['SRC']?>" alt="<?=$arItem['NAME']?>" class="article__img">
         <div class="article__inner">
             <p class="article__title"><?=$arItem['NAME']?></p>
