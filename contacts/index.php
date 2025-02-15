31669921579398a06c72406f0f05c1dab7f6b888
Daniil Neretin <dneretin01@gmail.com>
2025-02-16 01:46:18 +0300

    add contacts page

---
+++ contacts/index.php
@@ -0,0 +1,72 @@
+<?php
+
+/** @var \CMain $APPLICATION */
+
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
+$APPLICATION->SetTitle("Связаться с нами");
+?>
+    <div class="container">
+        <nav class="footer__nav contacts__nav">
+            <ul class="footer__list">
+                <li class="footer__item">
+                    <span class="footer__link">В мессенджерах</span>
+                </li>
+                <li class="footer__item">
+                    <a href="#" class="footer__link">Telegram</a>
+                </li>
+            </ul>
+            <ul class="footer__list">
+                <li class="footer__item">
+                    <span class="footer__link">Телефон</span>
+                </li>
+                <li class="footer__item">
+                    <a href="tel:+79996622835" class="footer__link">+7 999 662 28 35</a>
+                </li>
+            </ul>
+            <ul class="footer__list">
+                <li class="footer__item">
+                    <span class="footer__link">Часы работы</span>
+                </li>
+                <li class="footer__item">
+                    <span class="footer__link">Пн-Пт 10:00-19:00</span>
+                </li>
+            </ul>
+            <ul class="footer__list">
+                <li class="footer__item">
+                    <span class="footer__link">Эл. почта</span>
+                </li>
+                <li class="footer__item">
+                    <a href="mailto:info@vl28.pro" class="footer__link">info@vl28.pro</a>
+                </li>
+                <li class="footer__item contacts__item contacts__item_mobile">
+                    <span class="footer__link">По вопросам сотрудничества</span>
+                </li>
+                <li class="footer__item contacts__item_mobile">
+                    <a href="mailto:info@vl28.pro" class="footer__link">info@vl28.pro</a>
+                </li>
+            </ul>
+            <ul class="footer__list contacts__list_mobile">
+                <li class="footer__item">
+                    <span class="footer__link">По вопросам сотрудничества</span>
+                </li>
+                <li class="footer__item">
+                    <a href="mailto:info@vl28.pro" class="footer__link">info@vl28.pro</a>
+                </li>
+            </ul>
+            <ul class="footer__list">
+                <li class="footer__item">
+                    <span class="footer__link">Реквизиты</span>
+                </li>
+                <li class="footer__item">
+                    <span class="footer__link">ИП ЛЕОНОВ ВИТАЛИЙ ВИКТОРОВИЧ</span>
+                </li>
+                <li class="footer__item">
+                    <span class="footer__link">ИНН: 505011743603</span>
+                </li>
+                <li class="footer__item">
+                    <span class="footer__link">ОГРН/ОГРНИП: 324508100134856</span>
+                </li>
+            </ul>
+        </nav>
+    </div>
+<?php require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
