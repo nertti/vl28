5c391e8a4892a8c5ac76ffea51c87d585efd9870
Daniil Neretin <dneretin01@gmail.com>
2026-01-21 17:13:05 +0300

    del bottom menu item

--- .bottom3.menu.php
+++ .bottom3.menu.php
@@ -7,13 +7,13 @@ $aMenuLinks = Array(
 		Array(), 
 		"" 
 	),
-	Array(
-		"Instagram", 
-		"https://www.instagram.com/vl28.pro?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==", 
-		Array(), 
-		Array("description"=>"*компания meta признана в РФ экстремистской"), 
-		"" 
-	),
+//	Array(
+//		"Instagram",
+//		"https://www.instagram.com/vl28.pro?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==",
+//		Array(),
+//		Array("description"=>"*компания meta признана в РФ экстремистской"),
+//		""
+//	),
 	Array(
 		"Telegram", 
 		"https://t.me/vl28pro", 
