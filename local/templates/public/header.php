484714e4335d451dcb67cc93b51783df59c28506
Daniil Neretin <dneretin01@gmail.com>
2026-03-24 23:31:09 +0300

    fix product-main

--- local/templates/public/header.php
+++ local/templates/public/header.php
@@ -381,7 +381,7 @@ if ($isProfilePages && !$isFavoritePage){
             <?php $APPLICATION->IncludeComponent(
 	"bitrix:catalog.section",
 	"main",
-	array(
+	[
 		"ACTION_VARIABLE" => "action",
 		"ADD_PICT_PROP" => "IMAGES",
 		"ADD_PROPERTIES_TO_BASKET" => "Y",
@@ -405,29 +405,29 @@ if ($isProfilePages && !$isFavoritePage){
 		"CURRENCY_ID" => "RUB",
 		"CUSTOM_FILTER" => "{\"CLASS_ID\":\"CondGroup\",\"DATA\":{\"All\":\"AND\",\"True\":\"True\"},\"CHILDREN\":[]}",
 		"DATA_LAYER_NAME" => "dataLayer",
-		"DETAIL_URL" => "/catalog/#SECTION_CODE#/#ELEMENT_CODE#/",
+		"DETAIL_URL" => "",
 		"DISABLE_INIT_JS_IN_COMPONENT" => "N",
 		"DISCOUNT_PERCENT_POSITION" => "bottom-right",
 		"DISPLAY_BOTTOM_PAGER" => "Y",
 		"DISPLAY_TOP_PAGER" => "N",
-		"ELEMENT_SORT_FIELD" => "sort",
+		"ELEMENT_SORT_FIELD" => "active_from",
 		"ELEMENT_SORT_FIELD2" => "id",
-		"ELEMENT_SORT_ORDER" => "asc",
+		"ELEMENT_SORT_ORDER" => "desc",
 		"ELEMENT_SORT_ORDER2" => "desc",
 		"ENLARGE_PRODUCT" => "PROP",
 		"ENLARGE_PROP" => "COLLECTION",
 		"FILTER_NAME" => "arrFilter",
-		"HIDE_NOT_AVAILABLE" => "N",
-		"HIDE_NOT_AVAILABLE_OFFERS" => "N",
+		"HIDE_NOT_AVAILABLE" => "Y",
+		"HIDE_NOT_AVAILABLE_OFFERS" => "Y",
 		"IBLOCK_ID" => "2",
 		"IBLOCK_TYPE" => "rest_entity",
 		"INCLUDE_SUBSECTIONS" => "Y",
-		"LABEL_PROP" => array(
-		),
+		"LABEL_PROP" => [
+		],
 		"LABEL_PROP_MOBILE" => "",
 		"LABEL_PROP_POSITION" => "top-left",
 		"LAZY_LOAD" => "Y",
-		"LINE_ELEMENT_COUNT" => "4",
+		"LINE_ELEMENT_COUNT" => "3",
 		"LOAD_ON_SCROLL" => "N",
 		"MESSAGE_404" => "",
 		"MESS_BTN_ADD_TO_BASKET" => "В корзину",
@@ -438,33 +438,33 @@ if ($isProfilePages && !$isFavoritePage){
 		"MESS_NOT_AVAILABLE" => "Нет в наличии",
 		"META_DESCRIPTION" => "-",
 		"META_KEYWORDS" => "-",
-		"OFFERS_CART_PROPERTIES" => array(
+		"OFFERS_CART_PROPERTIES" => [
 			0 => "ARTNUMBER",
 			1 => "COLOR_REF",
 			2 => "SIZES_SHOES",
 			3 => "SIZES_CLOTHES",
-		),
-		"OFFERS_FIELD_CODE" => array(
+		],
+		"OFFERS_FIELD_CODE" => [
 			0 => "",
 			1 => "",
-		),
+		],
 		"OFFERS_LIMIT" => "5",
-		"OFFERS_PROPERTY_CODE" => array(
+		"OFFERS_PROPERTY_CODE" => [
 			0 => "COLOR_REF",
 			1 => "SIZES_SHOES",
 			2 => "SIZES_CLOTHES",
 			3 => "",
-		),
+		],
 		"OFFERS_SORT_FIELD" => "sort",
 		"OFFERS_SORT_FIELD2" => "id",
 		"OFFERS_SORT_ORDER" => "asc",
 		"OFFERS_SORT_ORDER2" => "desc",
 		"OFFER_ADD_PICT_PROP" => "",
-		"OFFER_TREE_PROPS" => array(
+		"OFFER_TREE_PROPS" => [
 			0 => "COLOR_REF",
 			1 => "SIZES_SHOES",
 			2 => "SIZES_CLOTHES",
-		),
+		],
 		"PAGER_BASE_LINK_ENABLE" => "N",
 		"PAGER_DESC_NUMBERING" => "N",
 		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
@@ -474,39 +474,38 @@ if ($isProfilePages && !$isFavoritePage){
 		"PAGER_TITLE" => "Товары",
 		"PAGE_ELEMENT_COUNT" => "6",
 		"PARTIAL_PRODUCT_PROPERTIES" => "N",
-		"PRICE_CODE" => array(
+		"PRICE_CODE" => [
 			0 => "BASE",
-		),
+		],
 		"PRICE_VAT_INCLUDE" => "Y",
 		"PRODUCT_BLOCKS_ORDER" => "price,props,sku,quantityLimit,quantity,buttons,compare",
 		"PRODUCT_DISPLAY_MODE" => "Y",
 		"PRODUCT_ID_VARIABLE" => "id",
-		"PRODUCT_PROPERTIES" => array(
+		"PRODUCT_PROPERTIES" => [
 			0 => "NEWPRODUCT",
 			1 => "MATERIAL",
-		),
+		],
 		"PRODUCT_PROPS_VARIABLE" => "prop",
 		"PRODUCT_QUANTITY_VARIABLE" => "",
-		"PRODUCT_ROW_VARIANTS" => "[{'VARIANT':'2','BIG_DATA':false}]",
+		"PRODUCT_ROW_VARIANTS" => "[{'VARIANT':'2','BIG_DATA':false},{'VARIANT':'2','BIG_DATA':false}]",
 		"PRODUCT_SUBSCRIPTION" => "Y",
-		"PROPERTY_CODE" => array(
+		"PROPERTY_CODE" => [
 			0 => "NEWPRODUCT",
 			1 => "",
-		),
-		"PROPERTY_CODE_MOBILE" => array(
-			0 => "IMAGES",
-		),
+		],
+		"PROPERTY_CODE_MOBILE" => [
+		],
 		"RCM_PROD_ID" => $_REQUEST["PRODUCT_ID"],
 		"RCM_TYPE" => "personal",
-		"SECTION_CODE" => $_REQUEST["SECTION_CODE"],
+		"SECTION_CODE" => "",
 		"SECTION_ID" => "",
 		"SECTION_ID_VARIABLE" => "SECTION_ID",
 		"SECTION_URL" => "",
-		"SECTION_USER_FIELDS" => array(
+		"SECTION_USER_FIELDS" => [
 			0 => "",
 			1 => "",
-		),
-		"SEF_MODE" => "Y",
+		],
+		"SEF_MODE" => "N",
 		"SET_BROWSER_TITLE" => "Y",
 		"SET_LAST_MODIFIED" => "N",
 		"SET_META_DESCRIPTION" => "Y",
@@ -516,7 +515,7 @@ if ($isProfilePages && !$isFavoritePage){
 		"SHOW_404" => "N",
 		"SHOW_ALL_WO_SECTION" => "Y",
 		"SHOW_CLOSE_POPUP" => "N",
-		"SHOW_DISCOUNT_PERCENT" => "N",
+		"SHOW_DISCOUNT_PERCENT" => "Y",
 		"SHOW_FROM_SECTION" => "N",
 		"SHOW_MAX_QUANTITY" => "N",
 		"SHOW_OLD_PRICE" => "N",
@@ -531,13 +530,11 @@ if ($isProfilePages && !$isFavoritePage){
 		"USE_PRODUCT_QUANTITY" => "N",
 		"COMPONENT_TEMPLATE" => "main",
 		"MESS_NOT_AVAILABLE_SERVICE" => "Недоступно",
-		"SEF_RULE" => "#SECTION_CODE#",
-		"SECTION_CODE_PATH" => "",
 		"DISPLAY_COMPARE" => "N"
-	),
-	false,
-	array('HIDE_ICONS' => 'Y')
+	],
+	false
 );?>
+
 
             <a href="/catalog/" class="products__link link">Перейти в каталог</a>
         </section>
